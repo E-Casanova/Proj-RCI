2aef5f89e250b706a78480d47daccaa24d42bd96
Eduardo Casanova <eduardo.casanova@tecnico.ulisboa.pt>
2024-03-19 22:05:37 +0000

    Updated Readme.md and made code prettier

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Proj-RCI
+# Chat Sobre Anel RCI
+
+## Descrição do Projeto
+Este projeto de Rede de Computadores e Internet (RCI) visa desenvolver uma aplicação de chat que opera dentro de uma topologia de rede em anel
+
+## Funcionalidades
+- **Topologia em Anel**: Implementação de uma rede em anel onde cada nó se conecta apenas ao seu sucessor e predecessor, formando um círculo de comunicação.
+- **Gerenciamento de Nós**: Capacidade de adicionar e remover nós da rede dinamicamente sem interromper o serviço de chat.
+- **Encaminhamento de Mensagens**: Utilização de um algoritmo de encaminhamento para determinar o caminho mais curto através do anel, otimizando a entrega de mensagens.
+- **Interface de Utilizador**: Comandos para permitir aos utilizadores juntarem-se à rede, enviarem mensagens e saírem do chat.
+
+## Começando
+### Pré-requisitos
+- GCC para compilação do código fonte
+- Sistema operacional Linux ou compatível
+
+## Comandos da Interface de Usuário
+- `join (j) ring id`: Entrar em um anel especificado com um ID único.
+- `direct join (dj) id succid succIP succTCP`: Entrar diretamente em um anel sem consultar o servidor de registros.
+- `chord (c)`: Estabelecer uma corda para otimizar o caminho de mensagens.
+- `leave (l)`: Sair do anel.
+- `message (m) dest message`: Enviar uma mensagem para um destino específico no anel.
+
+## Estrutura do Projeto
+- `user_commands.c`: Implementa funções para processar comandos do utilizador
+- `server_client.c`: Implementa a lógica de conexão entre nós (cliente/servidor).
+- `node.c`: Contém funções para gerenciamento de nós na rede em anel.
+- `main.c`: Ponto de entrada da aplicação.
--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+/*
+ * CHAT ON RING
+ * PROJETO RCI 2023/24 
+ * AUTORIA DE EDUARDO CASANOVA E EDUARDO SILVESTRE
+ *
+*/
+
 #include "utility.h"
 #include "node.h"
 #include "user_commands.h"
@@ -15,10 +22,6 @@ void usage(char * arg){
 }
 
 
-//PROJETO RCI 2023/24 
-//AUTORIA DE EDUARDO CASANOVA E EDUARDO SILVESTRE
-
-
 int main(int argc, char *argv[])
 {
 
@@ -26,7 +29,7 @@ int main(int argc, char *argv[])
     memset(&act, 0, sizeof act);
     act.sa_handler = SIG_IGN;
 
-    if(sigaction(SIGPIPE, &act, NULL) == -1) exit(1);
+    if(sigaction(SIGPIPE, &act, NULL) == -1) exit(1); // Ignoring SIGPIPE signal
 
     char ns_ip[INET_ADDRSTRLEN];
     char ns_port[6];
@@ -51,12 +54,14 @@ int main(int argc, char *argv[])
     if(isport(argv[2]) != 1) {
         printf("Invalid TCP port!\n");
         exit(1);
-    };
+    }
 
     if(argv[3] == NULL) {
         strcpy(ns_ip, DEFFAULT_NS_IP);
         strcpy(ns_port, DEFFAULT_NS_PORT);
-    } else if (isip(argv[3]) == 1) {
+
+    } 
+    if(isip(argv[3]) == 1) {
 
         strcpy(ns_ip, argv[3]);
         strcpy(ns_port, DEFFAULT_NS_PORT);
@@ -76,14 +81,13 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    if(system("clear") != 0) exit(EXIT_FAILURE);
+
     node_information * app_node = init_node(argv[1], argv[2], 1, ns_ip, ns_port);
-
-    //printf("Size of forwarding table: %ld\n bytes", sizeof(app_node->fwd_table));
-
 
     printf("\x1b[32m> Starting app @%s:%s\x1b[0m\n",app_node->ipaddr, app_node->port);
 
-    if (app_node == NULL) exit(1);
+    if (app_node == NULL) exit(EXIT_FAILURE);
 
     if(start_server_TCP(app_node) != 1){
         printf("\x1b[33m Error starting TCP server @ %s:%s\x1b[0m\n", app_node->ipaddr, app_node->port);
